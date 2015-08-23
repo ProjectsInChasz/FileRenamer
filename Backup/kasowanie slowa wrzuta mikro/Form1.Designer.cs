2078e4f6b7aa601f37e02147fe66d745b8d89999
korytkoBT <korytko07@gmail.com>
2015-08-23 18:34:43 +0200

    Local Copy

---
+++ Backup/kasowanie slowa wrzuta mikro/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace kasowanie_slowa_wrzuta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zmien = new System.Windows.Forms.Button();
+            this.wybor = new System.Windows.Forms.FolderBrowserDialog();
+            this.wyraz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // zmien
+            // 
+            this.zmien.Location = new System.Drawing.Point(47, 50);
+            this.zmien.Name = "zmien";
+            this.zmien.Size = new System.Drawing.Size(151, 44);
+            this.zmien.TabIndex = 3;
+            this.zmien.Text = "zmien nazwe";
+            this.zmien.UseVisualStyleBackColor = true;
+            this.zmien.Click += new System.EventHandler(this.zmien_Click);
+            // 
+            // wyraz
+            // 
+            this.wyraz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wyraz.Location = new System.Drawing.Point(47, 12);
+            this.wyraz.Name = "wyraz";
+            this.wyraz.Size = new System.Drawing.Size(163, 26);
+            this.wyraz.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 129);
+            this.Controls.Add(this.wyraz);
+            this.Controls.Add(this.zmien);
+            this.Name = "Form1";
+            this.Text = "usuwanie wrzuta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button zmien;
+        private System.Windows.Forms.FolderBrowserDialog wybor;
+        private System.Windows.Forms.TextBox wyraz;
+    }
+}
+
---
+++ Backup/kasowanie slowa wrzuta mikro/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kasowanie_slowa_wrzuta
+{
+    public partial class Form1 : Form
+    {
+        public string adres;
+        public int liczba;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void zmien_Click(object sender, EventArgs e)
+        {
+            wybor.ShowDialog();
+            adres = wybor.SelectedPath;
+            liczba = adres.Length;
+            string[] pliki = Directory.GetFiles(@adres);
+            foreach (string slowo in pliki)
+            {
+                FileInfo fin = new FileInfo(@slowo);
+                if (fin.Name.Contains(wyraz.Text))
+                {
+                fin.MoveTo(@slowo.Remove(liczba+1,wyraz.Text.Length));
+                }
+            }
+            MessageBox.Show("Zakonczone powodzeniem");
+        }
+
+    }
+}
